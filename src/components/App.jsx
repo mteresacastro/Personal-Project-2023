30f78a5bb54ed245ae795f4450a30e7b4c05eb0e
mteresacastro <m.teresacastrogonzalez@gmail.com>
2023-10-12 18:34:47 +0200

    add styles to hero

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,25 @@ function App() {
   return (
     <div>
       <header className="header">
-        <i className="fa-solid fa-bars"></i>
+        <i className="icon1 fa-solid fa-bars"></i>
         <img className="logo" src="/src/images/logofinal300.png" alt="logo k-dance war" />
-        <i className="fa-solid fa-user-large"></i>
+        <i className="icon2 fa-solid fa-user-large"></i>
+        <nav className="header__nav">
+          <ul className= "header__navUl">
+            <li>¿De qué va ésto?</li>
+            <li>Grupos</li>
+            <li>Reto</li>
+            <li>Ranking</li>
+            <li>Contacto</li>
+          </ul>
+        </nav>
       </header>
       <main>
         <div className="hero-opacity">
         <div className="hero">
-          <h1>Dancing to Glory</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis nisi fugiat pariatur dolores ab quae dolorum, quasi harum minima nostrum eum. Non modi nihil numquam, minima optio cum harum rerum.</p>
-          <button>Participa!</button>
+          <h1 className="hero__title">Dancing to Glory</h1>
+          <p className="hero__p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis nisi fugiat pariatur dolores ab quae dolorum, quasi harum minima nostrum eum. Non modi nihil numquam, minima optio cum harum rerum.</p>
+          <button className="hero__button"><a className="hero__a"href="">¡Participa!</a></button>
         </div>
         </div>
       </main>
