44f62642f897a76d3b04467c604e11689a86f5f4
mteresacastro <m.teresacastrogonzalez@gmail.com>
2023-10-13 21:02:53 +0200

    changes colour and add link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 // Fichero src/components/App.jsx
-import '../styles/App.scss';
+import '../styles/App.scss'
+import '../styles/core/variables.scss'
 
 function App() {
   return (
@@ -10,22 +11,26 @@ function App() {
         <i className="icon2 fa-solid fa-user-large"></i>
         <nav className="header__nav">
           <ul className= "header__navUl">
-            <li>¿De qué va ésto?</li>
-            <li>Grupos</li>
-            <li>Reto</li>
-            <li>Ranking</li>
-            <li>Contacto</li>
+            <li><a className= "header__navUl--li" href="">¿De qué va ésto?</a></li>
+            <li><a className= "header__navUl--li" href="">Grupos</a></li>
+            <li><a className= "header__navUl--li" href="">Reto</a></li>
+            <li><a className= "header__navUl--li" href="">Ranking</a></li>
+            <li><a className= "header__navUl--li" href="">Contacto</a></li>
           </ul>
         </nav>
       </header>
       <main>
-        <div className="hero-opacity">
+        <section className="hero-section">
         <div className="hero">
           <h1 className="hero__title">Dancing to Glory</h1>
           <p className="hero__p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis nisi fugiat pariatur dolores ab quae dolorum, quasi harum minima nostrum eum. Non modi nihil numquam, minima optio cum harum rerum.</p>
           <button className="hero__button"><a className="hero__a"href="">¡Participa!</a></button>
         </div>
-        </div>
+        </section>
+        <section>
+
+        </section>
+
       </main>
       <footer></footer>
     </div>
